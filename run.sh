ec8a60be9f12f9815fe29dd0841498109e68b296
xiongraorao <xiongraorao@gmail.com>
2016-12-19 11:22:02 +0800

    init_commit

---
+++ run.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+# -*- coding: UTF-8 -*-
+
+set -x
+
+if [[ $1 == "-a" ]]
+then
+	echo "start AnalyzerTopology named" $2
+	storm jar target/ever-1.0-SNAPSHOT.jar com.persist.VideoAnalyzer analyzer_config.json $2
+elif [[ $1 == "-d" ]]
+then
+	echo "start DetectTopology named" $2
+	storm jar Search.jar com.persist.DetectTopology detect_config.json $2
+elif [[ $1 == "-g" ]]
+then
+	echo "start GrabTopology named" $2
+	storm jar target/ever-1.0-SNAPSHOT.jar com.persist.VideoGrabber grabber_config.json $2
+elif [[ $1 == "-ic" ]]
+then
+	echo "start ImageCheck ServiceTopology named" $2
+	storm jar target/ever-1.0-SNAPSHOT.jar com.persist.ImageCheck check.json $2
+elif [[ $1 == "-iq" ]]
+then
+	echo "start ImageQuery ..."
+	storm jar target/ever-1.0-SNAPSHOT.jar com.persist.ImageQuery localhost 3772 image-check url_file.txt
+elif [[ $1 == "-rebuild" ]]
+then	echo "------rebuild this project..."
+	mvn clean install -DskipTests=true
+	mvn compile
+elif [[ $1 == "-pack" ]]
+then
+	echo "------pack this project to jar..."
+	mvn package
+elif [[ $1 == "-startKafka" ]]
+then
+	echo "start kafka service..."
+	cd /opt/software/kafka_2.11-0.10.0.0
+	sudo bin/kafka-server-start.sh config/server.properties&
+	cd -
+elif [[ $1 == "-stopKafka" ]]
+then
+	echo "stop kafka service..."
+	cd /opt/software/kafka_2.11-0.10.0.0
+	sudo bin/kafka-server-stop.sh config/server.properties
+	cd -
+elif [[ $1 == "-createTopic" ]]
+then
+	echo "create kafka Topic named" $2
+	cd /opt/software/kafka_2.11-0.10.0.0
+	bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partition 1 --topic $2
+	cd -
+elif [[ $1 == "-sendMessage" ]]
+then
+	echo "Send KafkaMessage to topic named" $2
+	cd /opt/software/kafka_2.11-0.10.0.0
+	bin/kafka-console-producer.sh --broker-list localhost:9092 --topic $2
+elif [[ $1 == "-consumeMessage" ]]
+then
+	echo "Consume KafkaMessage of topic named" $2
+	cd /opt/software/kafka_2.11-0.10.0.0
+	bin/kafka-console-consumer.sh --zookeeper localhost:9092 --topic $2 --from-beginning
+elif [[ $1 == "-deleteTopic" ]]
+then
+	echo "DeleteTopic named" $2
+	cd /opt/software/kafka_2.11-0.10.0.0
+	bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic $2
+	cd -
+elif [[ $1 == "-listTopic" ]]
+then
+	echo "view topics..."
+	cd /opt/software/kafka_2.11-0.10.0.0
+	bin/kafka-topics.sh --list --zookeeper localhost:2181
+	cd -
+else
+	echo "Usage:"
+	echo "-g topology_name"
+	echo "	start GrabTopology"
+	echo "-a topology_name"
+	echo "	start AnalyzerTopology"
+	echo "-d topology_name"
+	echo "	start DetectTopology"
+	echo "-ic topology_name"
+	echo "	start ImageCheck service"
+	echo "-iq"
+	echo "	execute ImageQuery client"
+	echo "-rebuild"
+	echo "	clean and install project using mvn"
+	echo "-pack"
+	echo "	pack the project to jar file"
+	echo "-createTopic topic_name"
+	echo "	create kafka topic"
+	echo "-sendMessage topic_name"
+	echo "	send message to this specifide topic"
+	echo "-deleteTopic topic_name"
+	echo "	delete kafkatopic"
+	echo "-listTopic"
+	echo "	view all kafka topics"
+	
+fi
+
