f3411b4967f4f5138b8b6836976f623a88447115
xiongraorao <xiongraorao@gmail.com>
2016-12-20 17:46:54 +0800

    完成了 HDFS 文件读取测试

---
+++ src/main/java/com/xrr/test/HDFSTest.java
@@ -0,0 +1,26 @@
+package com.xrr.test;
+
+import com.persist.util.helper.HDFSHelper;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.util.Progressable;
+import java.io.*;
+import java.net.URI;
+import java.lang.String;
+
+public class HDFSTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HDFSHelper hh=new HDFSHelper("hdfs://localhost:9000/user/hadoop");
+		hh.upload("/home/ubuntu/mbuntu-0.jpg", "1.jpg");
+		hh.close();
+		hh.download("/home/hadoop/mbuntu-0-from-hdfs.jpg", "1.jpg");
+		hh.close();
+	}
+
+}
